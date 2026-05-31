3530624d3bf6fadef4608498dba3891e5bbdd1d3
Rajesh Prasad <89001548+rajesh-kumar-prasad@users.noreply.github.com>
2026-05-31 12:23:15 +0530

    Microsoft.ML

---
+++ MLDOTNET_PROJ/MLDOTNET_PROJ/PizzaInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLDOTNET_PROJ
+{
+    public class PizzaInfo
+    {
+        public float OvenTemp { get; set; }
+        public float CookingTime { get; set; }
+    }
+
+    public class PredictedCookingTime
+    {
+        public float Score { get; set; }
+    }
+}
---
+++ MLDOTNET_PROJ/MLDOTNET_PROJ/Program.cs
@@ -0,0 +1,16 @@
+﻿using MLDOTNET_PROJ;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var data = new[]
+        {
+            new PizzaInfo(){OvenTemp=180, CookingTime=15},
+            new PizzaInfo(){OvenTemp=200, CookingTime=12},
+            new PizzaInfo(){OvenTemp=220, CookingTime=10},
+            new PizzaInfo(){OvenTemp=250, CookingTime=8}
+        };
+
+    }
+}
